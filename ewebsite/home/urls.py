fe3ee530395fd9cc20ad1d4a9f1b79be5cf002e4
BenjaminKoshyBiju <bennn455@gmail.com>
2023-07-16 20:14:38 +0530

    was able to transfer data from home to details page

--- ewebsite/home/urls.py
+++ ewebsite/home/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('shop',views.shop,name='shop'),
     path('contact',views.contact,name='contact'),
     path('checkout',views.checkout,name='checkout'),
-    path('detail',views.detail,name='detail'),
+    path('detail/<int:table1_id>/',views.detail,name='detail'),
     path('cart',views.cart,name='cart'),
      
 ]
--- ewebsite/home/views.py
+++ ewebsite/home/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Table1,Table2
+
 # Create your views here.
 table1_data = Table1.objects.all()  
 table2_data = Table2.objects.all()
 def index(request):
     return render(request,'index.html',{
         'table1_data': table1_data,
-        'table2_data': table2_data,
+      
         })
 def shop(request):
     return render(request,'shop.html')
-def detail(request):
-    return render(request,'detail.html')
+def detail(request,table1_id):
+    data = get_object_or_404(Table1, id=table1_id)
+    table2_data=get_object_or_404(Table2, id=table1_id)
+    return render(request,'detail.html',{'data': data,'table2_data': table2_data})
 def cart(request):
     return render(request,'cart.html')
 def checkout(request):
