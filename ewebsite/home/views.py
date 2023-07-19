6c9d3d154b82601aa0eeaebaff010b3f79332b00
BenjaminKoshyBiju <bennn455@gmail.com>
2023-07-20 01:15:52 +0530

    working on crud functions

--- ewebsite/home/views.py
+++ ewebsite/home/views.py
@@ -4,6 +4,7 @@ from .models import Table1,Table2,Cart
 # Create your views here.
 table1_data = Table1.objects.all()  
 table2_data = Table2.objects.all()
+
 def index(request):
     return render(request,'index.html',{
         'table1_data': table1_data,
@@ -14,19 +15,24 @@ def shop(request):
 def detail(request,table1_id):
     data = get_object_or_404(Table1, id=table1_id)
     table2_data=get_object_or_404(Table2, id=table1_id)
+
     if request.method=='POST':
         name = data.name
         price = data.price
         img = data.img
-        
-        cart = Cart(name=name,price=price,img=img)
+        total=price
+        cart = Cart(name=name,price=price,img=img,total=total)
         cart.save()
         print("saved successfully")
         return redirect('cart.html')
     else:     
         return render(request,'detail.html',{'data': data,'table2_data': table2_data})
 def cart(request,table1_id):
-    return render(request,'cart.html')
+    table3_data = get_object_or_404(Cart, id=table1_id)
+    carts = Cart.objects.all()
+    total=table3_data.price*table3_data.count
+    
+    return render(request,'cart.html',{'data': table3_data,'total':total, 'carts':carts })
 def checkout(request):
     return render(request,'checkout.html')
 def contact(request):
